a3e0cd5f66c5e1c5727cd96900c542eaa4059e33
Yaourt <yaourt@yaourtprod.net>
2013-11-28 15:13:30 +0100

    BoxDirectoryWatcher code base

---
+++ gtk3linuxboxsync/BoxDirectoryWatcher.py
@@ -0,0 +1,34 @@
+__author__ = 'yaourt'
+
+import subprocess
+import os
+
+import pyinotify
+
+
+class BoxDirectoryWatcher:
+    """Manage the event related to the user Box directory"""
+
+    def __init__(self, eventProcessor):
+        self.eventProcessor = eventProcessor
+        desktop_dir = subprocess.check_output(['xdg-user-dir', 'DESKTOP']).strip()
+        self.box_dir = os.path.join(desktop_dir, 'Box')
+        if not os.path.exists(self.box_dir):
+            os.makedirs(self.box_dir)
+
+        self.watch_manager = pyinotify.WatchManager()
+        self.mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+        BoxDirectoryWatcher._instance = self
+
+    def start(self):
+        """Start the watcher"""
+        notifier = pyinotify.ThreadedNotifier(self.watch_manager, self.eventProcessor)
+        wdd = self.watch_manager.add_watch(self.box_dir, self.mask, rec=True, auto_add=True)
+        self.watch_descriptor = wdd
+        self.notifier = notifier
+        notifier.start()
+
+    def stop(self):
+        """Stop the watcher"""
+        self.watch_manager.rm_watch(self.watch_descriptor[self.box_dir], rec=True)
+        self.notifier.stop()
