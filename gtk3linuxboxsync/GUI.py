c6d126ee6a41a2e6071bc3f453cb27336ef37292
Yaourt <yaourt@yaourtprod.net>
2013-11-30 18:55:35 +0100

    Login webview with final oauth response handler skeleton: done

--- gtk3linuxboxsync/GUI.py
+++ gtk3linuxboxsync/GUI.py
@@ -1,17 +1,23 @@
 __author__ = 'yaourt'
 
 from gi.repository import Gtk, WebKit
+import urllib
 
 class GUI:
     def __init__(self):
         w = Gtk.Window()
+        w.set_title("Box.com login")
+        w.set_position(Gtk.WindowPosition.CENTER)
         v = WebKit.WebView()
+        self.v = v
+        self.w = w
         v.can_go_back_or_forward(False)
         sw = Gtk.ScrolledWindow()
         w.add(sw)
+        self.sw = sw
         sw.add(v)
-        w.set_size_request(400, 700)
-        w.connect("destroy", lambda q: Gtk.main_quit())
+        w.set_size_request(400, 666)
+        w.connect("delete-event", Gtk.main_quit)
         v.connect("resource-request-starting", self.res_change)
         v.connect("context-menu", self.disable_context_menu)
         v.load_uri('http://127.0.0.1:8080')
@@ -20,12 +26,14 @@ class GUI:
 
     def res_change(self, webview, webframe, webresource, req, resp=None, data=None):
         uri = req.get_uri()
-        if uri.startswith('oauth://'):
-            state = uri[len('oauth://'):]
-            print "State: %s" % (state)
-            req.set_uri("about:blank")
+        if uri.startswith('oauth:'):
+            state = uri[len('oauth:'):]
+            print "State: %s" % (urllib.unquote(state))
+            req.set_uri('about:blank')
+            self.sw.remove(self.v)
 
     def disable_context_menu(webview, default_menu, hit_test_result, keyboard_triggered, data):
         return True
+
 
 g = GUI()
